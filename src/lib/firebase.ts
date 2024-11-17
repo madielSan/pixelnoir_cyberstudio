ee073ba4fb328bb56ed40ef12b3fc30f57632eb0
elmadiF <84844658+madielSan@users.noreply.github.com>
2024-11-17 16:25:55 +0100

    Firebase resolve

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,19 +6,19 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_AIzaSyCm85ZF9m5WiiGCfWgTmc520oeMqKUBYFs,
-  authDomain: import.meta.env.VITE_pixel-noir-cyberstudio.firebaseapp.com,
-  projectId: import.meta.env.VITE_pixel-noir-cyberstudio,
-  storageBucket: import.meta.env.VITE_pixel-noir-cyberstudio.appspot.com,
-  messagingSenderId: import.meta.env.VITE_733165666661,
-  appId: import.meta.env.VITE_1:733165666661:web:283215e1b2cb5469bb218c,
-  measurementId: import.meta.env.VITE_G-3TWL3JN2JV
+  apiKey: "AIzaSyCm85ZF9m5WiiGCfWgTmc520oeMqKUBYFs",
+  authDomain: "pixel-noir-cyberstudio.firebaseapp.com",
+  projectId: "pixel-noir-cyberstudio",
+  storageBucket: "pixel-noir-cyberstudio.appspot.com",
+  messagingSenderId: "733165666661",
+  appId: "1:733165666661:web:283215e1b2cb5469bb218c",
+  measurementId: "G-3TWL3JN2JV"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const storage = getStorage(app, "gs://pixel-noir-cyberstudio.appspot.com");
+export const storage = getStorage(app, "gs://pixel-noir-cyberstudio.firebasestorage.app");
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
 export const functions = getFunctions(app);
@@ -26,6 +26,3 @@ export const functions = getFunctions(app);
 auth.useDeviceLanguage();
 
 export default app;
-
-console.log("Firebase Config :", firebaseConfig);
-
