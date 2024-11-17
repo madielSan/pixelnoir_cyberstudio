bcd1ca21f3e5ae56a47457f4e8d4ce4b784506ac
elmadiF <84844658+madielSan@users.noreply.github.com>
2024-11-17 14:50:35 +0100

    Variables d'environnement

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm85ZF9m5WiiGCfWgTmc520oeMqKUBYFs",
-  authDomain: "pixel-noir-cyberstudio.firebaseapp.com",
-  projectId: "pixel-noir-cyberstudio",
-  storageBucket: "pixel-noir-cyberstudio.appspot.com",
-  messagingSenderId: "733165666661",
-  appId: "1:733165666661:web:283215e1b2cb5469bb218c",
-  measurementId: "G-3TWL3JN2JV"
+  apiKey: import.meta.env.VITE_AIzaSyCm85ZF9m5WiiGCfWgTmc520oeMqKUBYFs,
+  authDomain: import.meta.env.VITE_pixel-noir-cyberstudio.firebaseapp.com,
+  projectId: import.meta.env.VITE_pixel-noir-cyberstudio,
+  storageBucket: import.meta.env.VITE_pixel-noir-cyberstudio.appspot.com,
+  messagingSenderId: import.meta.env.VITE_733165666661,
+  appId: import.meta.env.VITE_1:733165666661:web:283215e1b2cb5469bb218c,
+  measurementId: import.meta.env.VITE_G-3TWL3JN2JV
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,3 +26,6 @@ export const functions = getFunctions(app);
 auth.useDeviceLanguage();
 
 export default app;
+
+console.log("Firebase Config :", firebaseConfig);
+
